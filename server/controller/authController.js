da9aa3d0817d7889e279dccff38d3c7313864c51
Baruch <baruchefiok@gmail.com>
2025-06-01 15:52:15 +0100

    Fixed issues relating to cookie
    Update cookie settings in login function to allow cross-site requests
    
    Update start script in package.json to set NODE_ENV to production

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -97,9 +97,11 @@ export const login = async (req, res) => {
     res.cookie("userSession", token, {
       maxAge: 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
       secure: process.env.NODE_ENV !== "development",
     });
+
+    console.log("ENV IS: ", process.env.NODE_ENV);
 
     return res.status(200).json({
       success: true,
